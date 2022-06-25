c9ecc31fa3e645f35905ad49a7f5f4dc433d0295
Pavel Fronchik <ridan999@yandex.ru>
2022-06-25 21:53:41 +0300

    4redFsddffddtddflfdd

--- scripts/validate.js
+++ scripts/validate.js
@@ -59,10 +59,8 @@ export default class FormValidator {
   toggleButtonState = () => {
     if (this._hasInvalidInput()) {
       this._buttonElement.classList.add(this._obj.inactiveButtonClass);
-      this._buttonElement.setAttribute('disabled', 'disabled');
     } else {
       this._buttonElement.classList.remove(this._obj.inactiveButtonClass);
-      this._buttonElement.removeAttribute('disabled');
     }
   };
   resetButtonSave = () => {
